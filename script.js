1487efd378ebd75d31f73f6bcc0a999ca70e7a70
Rob Hingstrum <rhingstrumjr@gmail.com>
2021-12-13 22:04:48 -0600

    setup index.html, styles.css, and script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// On page load, create a 50 sided grid
+// The cells have a class called sketchCell
+// Width and height will be 500 / number per side
+// hovering over the cell will cause it to change color
